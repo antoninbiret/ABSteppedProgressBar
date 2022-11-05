82cdea182e51880ebf9fc282015e925794802cdf
Pran Kishore <kishorepran@gmail.com>
2022-11-05 21:14:40 +0530

    Correct Package.Swift format
    Correct Package.Swift format mind the space in the swift-tools-version version
    Commas are needed at the end
    dependencies must come before exclusion
    set the library type to dynamic

--- Package.swift
+++ Package.swift
@@ -1,15 +1,16 @@
-// swift-tools-version: 5.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "ABSteppedProgressBar",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v9),
     ],
     products: [
         .library(
             name: "ABSteppedProgressBar",
+            type: .dynamic,
             targets: ["ABSteppedProgressBar"]),
     ],
     dependencies: [
@@ -19,9 +20,9 @@ let package = Package(
     targets: [
         .target(
             name: "ABSteppedProgressBar",
+            dependencies: [],
             path: "Sources",
-            exclude: ["Exemple-ABSteppedProgressBar", "ABSteppedProgressBar.podspec", ],
-            dependencies: [])
+            exclude: ["Exemple-ABSteppedProgressBar", "ABSteppedProgressBar.podspec"]),
     ],
     swiftLanguageVersions: [.v5]
 )
